d3a833b3760b56ee7312ce24a98331ffd70735dd
SquaredOnion <rokenzo88.rk@gmail.com>
2021-11-15 19:58:48 -0300

    Recent Ocurrencies component

--- src/components/Ocurrency/index.js
+++ src/components/Ocurrency/index.js
@@ -92,6 +92,90 @@ const Ocurrencies = () => {
                             </div>
                         </td>
                     </tr>
+                    <br />
+                    <tr>
+                        <td className={borderStyle}>
+                            <div>
+                                <h3>Ocorrência 5</h3>
+                                <hr />
+                                <table className={ocurrencyStyle}>
+                                    <tr>
+                                        <td className={spacing}>Tipo de Ocorrência: Assalto</td>
+                                        <td className={spacing}>Número de Envolvidos: 2</td>
+                                        <td className={spacing}>Houve Feridos: Não</td>
+                                    </tr>
+                                    <tr>
+                                        <td className={spacing}>Cidade: São Paulo</td>
+                                        <td className={spacing}>Endereço: Bairro C, nº 540</td>
+                                        <td className={spacing}>Horário: 21:23:54</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </td>
+                    </tr>
+                    <br />
+                    <tr>
+                        <td className={borderStyle}>
+                            <div>
+                                <h3>Ocorrência 6</h3>
+                                <hr />
+                                <table className={ocurrencyStyle}>
+                                    <tr>
+                                        <td className={spacing}>Tipo de Ocorrência: Assalto</td>
+                                        <td className={spacing}>Número de Envolvidos: 4</td>
+                                        <td className={spacing}>Houve Feridos: Sim</td>
+                                    </tr>
+                                    <tr>
+                                    <td className={spacing}>Cidade: São Bernardo do Campo</td>
+                                        <td className={spacing}>Endereço: Bairro B, nº 1900</td>
+                                        <td className={spacing}>Horário: 08:43:25</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </td>
+                    </tr>
+                    <br />
+                    <tr>
+                        <td className={borderStyle}>
+                            <div>
+                                <h3>Ocorrência 7</h3>
+                                <hr />
+                                <table className={ocurrencyStyle}>
+                                    <tr>
+                                        <td className={spacing}>Tipo de Ocorrência: Acidente de Trânsito</td>
+                                        <td className={spacing}>Número de Envolvidos: 6</td>
+                                        <td className={spacing}>Houve Feridos: Sim</td>
+                                    </tr>
+                                    <tr>
+                                    <td className={spacing}>Cidade: São Paulo</td>
+                                        <td className={spacing}>Endereço: Bairro A, nº 180</td>
+                                        <td className={spacing}>Horário: 19:13:59</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </td>
+                    </tr>
+                    <br />
+                    <tr>
+                        <td className={borderStyle}>
+                            <div>
+                                <h3>Ocorrência 8</h3>
+                                <hr />
+                                <table className={ocurrencyStyle}>
+                                    <tr>
+                                        <td className={spacing}>Tipo de Ocorrência: Incêndio</td>
+                                        <td className={spacing}>Motivo do incêndio: Desconhecido</td>
+                                        <td className={spacing}>Houve Feridos: Não</td>
+                                    </tr>
+                                    <tr>
+                                        <td className={spacing}>Cidade: Atibaia</td>
+                                        <td className={spacing}>Endereço: Bairro B, nº 780</td>
+                                        <td className={spacing}>Horário: 16:26:24</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </td>
+                    </tr>
                 </table>
             </div>
           </td>
--- src/components/Recent/index.js
+++ src/components/Recent/index.js
@@ -4,7 +4,10 @@ import {
   component, 
   recentOcurrency1, 
   recentOcurrency2, 
-  recentOcurrency3
+  recentOcurrency3,
+  hrStyle,
+  recOcurrencyStyle,
+  details
 } from './recent.module.scss'
 
 const Recent = () => {
@@ -17,17 +20,53 @@ const Recent = () => {
                 <ul>
                     <li>
                       <div className={recentOcurrency1}>
-                        Ocorrência 1
+                        <div>
+                            <h2>Ocorrência 1</h2>
+                            <hr className={hrStyle}/>
+                            <div className={recOcurrencyStyle}>
+                              <p>Tipo de Ocorrência: Sequestro</p>
+                              <p>Número de envolvidos: Desconhecido</p>
+                              <p>Número de vítimas: 1</p>
+                              <p>Cidade: São Paulo</p>
+                              <p>Endereço: Avenida B, 216</p>
+                              <p>Horário: 03:22:45</p>
+                              <p className={details}>Mais Detalhes: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.</p>
+                            </div>
+                        </div>
                       </div>
                     </li>
                     <li>
                       <div className={recentOcurrency2}>
-                      Ocorrência 2
+                        <div>
+                          <h2>Ocorrência 2</h2>
+                          <hr className={hrStyle}/>
+                          <div className={recOcurrencyStyle}>
+                              <p>Tipo de Ocorrência: Furto</p>
+                              <p>Número de envolvidos: 2</p>
+                              <p>Número de vítimas: 5</p>
+                              <p>Cidade: Jundiaí</p>
+                              <p>Endereço: Avenida C, 130</p>
+                              <p>Horário: 14:03:18</p>
+                              <p className={details}>Mais Detalhes: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.</p>
+                            </div>
+                        </div>
                       </div>
                     </li>
                     <li>
                       <div className={recentOcurrency3}>
-                      Ocorrência 3
+                        <div>
+                          <h2>Ocorrência 3</h2>
+                          <hr className={hrStyle}/>
+                          <div className={recOcurrencyStyle}>
+                              <p>Tipo de Ocorrência: Estelionato</p>
+                              <p>Número de envolvidos: Desconhecido</p>
+                              <p>Número de vítimas: 1</p>
+                              <p>Cidade: São Paulo</p>
+                              <p>Endereço: Desconhecido</p>
+                              <p>Horário: 16:23:43</p>
+                              <p className={details}>Mais Detalhes: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.</p>
+                            </div>
+                        </div>
                       </div>
                     </li>
                 </ul>
